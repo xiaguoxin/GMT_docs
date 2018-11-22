e0cc2e9cff695be728ed56bb4a34c76bfa9a2444
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-22 14:55:09 +0000

    Deploy gmt-china/GMT_docs to github.com/gmt-china/GMT_docs.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '5.4.4',
+    LANGUAGE: 'zh_CN',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
